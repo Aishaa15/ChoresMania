581cb42ce4c2f71092aabdb0998f7705d4a9d64b
armena124 <128395631+armena124@users.noreply.github.com>
2023-06-15 21:30:59 -0400

    Update login.java

--- login.java
+++ login.java
@@ -4,52 +4,72 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
 import java.awt.*;
 
 
-public class Themainpage extends JFrame
+public class login extends Themainpage
 {
-        
-    public Themainpage()
+    //create login page
+    public login()
     {
-        //creating main window
-        JFrame window = new JFrame("Chores Mania");
-        window.setSize(1000,1000);
+      //make login window
+      JFrame window2 = new JFrame("Login");
+      window2.setSize(1000,1000);
+      
+      //set background color
+      Container dp = window2.getContentPane();
+      dp.setBackground(new Color(247, 205, 208));
+      
+      //Create header label
+      JLabel header = new JLabel("Login");
+      header.setBounds(450,100,500,80);
+      header.setFont(new Font("Serif", Font.BOLD,55));
+      window2.add(header);
+     
+      //create username label
+      JLabel user_name = new JLabel("Username:");
+      user_name.setBounds(380, 300, 200, 30);
+      user_name.setFont(new Font("", Font.BOLD, 20));
+      window2.add(user_name);
+     
+      //create textfield for username
+      JTextField username = new JTextField();
+      username.setBounds(500,300,200,40);
+      window2.add(username);
 
-        //set the background colour
-        Container cp = window.getContentPane();
-        cp.setBackground(new Color(247, 205, 208));
+      //create password label
+      JLabel pass_word = new JLabel("Password:");
+      pass_word.setBounds(380,400,200,30);
+      pass_word.setFont(new Font("", Font.BOLD,20));
+      window2.add(pass_word);
 
-        //add title image
-        ImageIcon image = new ImageIcon("chores.png");
-        JLabel label_image = new JLabel(image);
-        label_image.setBounds(180, 50, 600, 500);
-        window.add(label_image);
+      //create passwordfield
+      JPasswordField password = new JPasswordField();
+      password.setBounds(500,400,200,40);
+      window2.add(password);
 
-        //create login button 
-        JButton Login = new JButton("Login");
-        Login.setBounds(380,550,250,40);
-        window.add(Login);
+      //create login button 
+      JButton login_button = new JButton("Login");
+      login_button.setBounds(500,550,100,40);
+      window2.add(login_button);
 
-        //connect login button to login window
-        Login.addActionListener(new ActionListener()
+      //add actionlistener 
+      login_button.addActionListener(new ActionListener()
+      {
+        public void actionPerformed(ActionEvent e)
         {
-            public void actionPerformed(ActionEvent e)
-            {
-               window.dispose();
-               login menu = new login();
-            }
-        });
+               
+        }
+      });
+
+      window2.setLayout(null);
+      window2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      window2.setVisible(true);
+      window2.setResizable(false);
 
 
-        //create sign up button
-        JButton signup = new JButton("Sign up");
-        signup.setBounds(380,650,250,40);
-        window.add(signup);
-
-        window.setLayout(null);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(false);
-        window.setVisible(true);
     }
 }
