b1749f4b29de0e62bc59f9ba0684e005e5dbaafc
Aisha <128395400+Aishaa15@users.noreply.github.com>
2023-06-16 15:47:03 +0000

    final signup page functionality update

---
+++ main.java
@@ -0,0 +1,11 @@
+Public class main
+{
+    public static void main(String[] args) 
+    {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new SignUpPage();
+            }
+        });
+    }
+}
