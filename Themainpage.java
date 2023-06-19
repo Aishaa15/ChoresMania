f779e34d3ab3a17307f491b787efdf1c1a5defd7
armena124 <128395631+armena124@users.noreply.github.com>
2023-06-18 20:22:19 -0400

    Update Themainpage.java

--- Themainpage.java
+++ Themainpage.java
@@ -1,17 +1,22 @@
+package choresmania;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.*;
 
+
 public class Themainpage extends JFrame
 {
-     //creating the window
-     public Themainpage()
-     {
+        
+    public Themainpage()
+    {
+        //creating main window
         JFrame window = new JFrame("Chores Mania");
         window.setSize(1000,1000);
-
+ 
         //set the background colour
         Container cp = window.getContentPane();
         cp.setBackground(new Color(247, 205, 208));
@@ -27,14 +32,34 @@ public class Themainpage extends JFrame
         Login.setBounds(380,550,250,40);
         window.add(Login);
 
+        //connect login button to login window
+        Login.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+              login menu = new login();
+            
+            }
+        });
+
+
         //create sign up button
         JButton signup = new JButton("Sign up");
         signup.setBounds(380,650,250,40);
         window.add(signup);
 
+        signup.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                window.dispose();
+                SignUpPage menu = new SignUpPage();
+            }
+        });
+
         window.setLayout(null);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setVisible(true);
-     }
+    }
 }
