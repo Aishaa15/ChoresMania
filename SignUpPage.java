9cf1eba7865006ebb09eea01585cd8009efbaf40
liujulie13 <128396514+liujulie13@users.noreply.github.com>
2023-06-20 11:14:52 -0400

    Update SignUpPage.java

--- SignUpPage.java
+++ SignUpPage.java
@@ -1,9 +1,8 @@
+package choresmania;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -24,13 +23,13 @@ public class SignUpPage extends JFrame {
     private JTextField securityQuestionField;
     private JTextField securityAnswerField;
 
-    private Login loginPage;
+    private login loginPage;
 
     public SignUpPage() {
         initializeUI();
     }
 
-    public void setLoginPage(Login loginPage) {
+    public void setLoginPage(login loginPage) {
         this.loginPage = loginPage;
     }
 
@@ -146,7 +145,7 @@ public class SignUpPage extends JFrame {
             securityAnswerField.setText("");
 
             // Create an instance of the login page and pass the file name
-            Login login = new Login("user_information.txt");
+            login login = new login("user_information.txt");
             login.setVisible(true);
         }
     }
@@ -159,10 +158,13 @@ public class SignUpPage extends JFrame {
             writer.println("Name: " + name);
             writer.println("Username: " + username);
             writer.println("Password: " + password);
+            writer.println(username + " Login: false");
             writer.println("Email: " + email);
             writer.println("Security Question: " + securityQuestion);
             writer.println("Security Answer: " + securityAnswer);
-            writer.println();
+            writer.println(username + " Chores: a, b, c, d");
+            writer.println(username + " Grocery list: a, b, c, d");
+            writer.println("");
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
