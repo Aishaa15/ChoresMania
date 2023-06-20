35720fa642fc371d5fc0d46dc828576e4d24cc29
armena124 <128395631+armena124@users.noreply.github.com>
2023-06-19 22:12:12 -0400

    Create ChoresRandomizer.java

---
+++ ChoresRandomizer.java
@@ -0,0 +1,18 @@
+package choresmania;
+
+import javax.swing.JFrame;
+
+public class ChoresRandomizer 
+{
+    public ChoresRandomizer()
+    {
+      JFrame window5 = new JFrame("Duty Assignment");
+      window5.setSize(1500,1500);
+
+      window5.setLayout(null);
+      window5.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      window5.setVisible(true);
+      window5.setResizable(false);
+    }
+     
+}
