882c83289d186d6e26c22ba04a2589b21b1b5643
armena124 <128395631+armena124@users.noreply.github.com>
2023-06-20 12:12:01 -0400

    Update ChoresRandomizer.java

--- ChoresRandomizer.java
+++ ChoresRandomizer.java
@@ -1,8 +1,13 @@
+package choresmania;
+
+import java.awt.Color;
+import java.awt.Container;
 import java.awt.Font;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
 
 public class ChoresRandomizer 
 {
@@ -14,16 +19,53 @@ public class ChoresRandomizer
       Container rp = window5.getContentPane();
       rp.setBackground(new Color(247, 205, 208));
       
-      ImageIcon person1 = new ImageIcon("/Users/armee/choresmania/demo/src/main/java/choresmania/catfriends.png");
-      JLabel label_person1 = new JLabel(person1);
-      label_person1.setBounds(20,80, 1200, 1500);
-      window5.add(label_person1);
+     ImageIcon persons = new ImageIcon("/Users/armee/choresmania/demo/src/main/java/choresmania/catto.png");
+     JLabel label_persons = new JLabel(persons);
+     label_persons.setBounds(50,80, 1600, 1500);
+     window5.add(label_persons);
 
-      JLabel chorelabel = new JLabel("----CHORES----");
-      chorelabel.setBounds(1200, 100, 200, 30);
-      chorelabel.setFont(new Font("", Font.BOLD, 20));
-      window5.add(chorelabel);
+      JLabel person1 = new JLabel("Person #1");
+      person1.setBounds(320,300,180,40);
+      person1.setFont(new Font("Serif", Font.BOLD, 20));
+      window5.add(person1);
 
+      //make label for person1
+      JLabel person1_label = new JLabel("chore...");//add chore randomized here
+      person1_label.setBounds(320,350,200,40);
+      person1_label.setFont(new Font("Serif", Font.BOLD, 20));
+      window5.add(person1_label);
+
+      JLabel person2 = new JLabel("Person #2");
+      person2.setBounds(965,300,180,40);
+      person2.setFont(new Font("Serif", Font.BOLD, 20));
+      window5.add(person2);
+
+      JLabel person2_label = new JLabel("chore...");
+      person2_label.setBounds(965,350,200,40);
+      person2_label.setFont(new Font("Serif", Font.BOLD, 20));
+      window5.add(person2_label);
+
+      JLabel person3 = new JLabel("Person #3");
+      person3.setBounds(320,780,180,40);
+      person3.setFont(new Font("Serif", Font.BOLD, 20));
+      window5.add(person3);
+
+      JLabel person3_label = new JLabel("chore...");
+      person3_label.setBounds(320,830,200,40);
+      person3_label.setFont(new Font("Serif", Font.BOLD, 20));
+      window5.add(person3_label);
+
+      JLabel person4 = new JLabel("Person #4");
+      person4.setBounds(965,780,180,40);
+      person4.setFont(new Font("Serif", Font.BOLD, 20));
+      window5.add(person4);
+
+      JLabel person4_label = new JLabel("chore...");
+      person4_label.setBounds(965,830,200,40);
+      person4_label.setFont(new Font("Serif", Font.BOLD, 20));
+      window5.add(person4_label);
+
+      
 
 
       window5.setLayout(null);
